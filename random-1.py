70131d0afccd525ec5709cb45989f0b3e67d2971
Jens Andreasson <jens.andreasson@ntig.se>
2024-09-05 09:57:50 +0200

    spellogik Lowes spel

--- random-1.py
+++ random-1.py
@@ -1,6 +1,11 @@
 # # bestäm om det är udda eller jämnt
 # # rulla tärningar
 # # vinnaren är det som bestämts
+# OM udda
+# undersök om datorns tärning är udda
+# undersök om spelarens tärning är udda
+
+
 # from random import randint
 
 # rule = input("Skriv om ni spelar udda eller jämnt:")
@@ -25,17 +30,25 @@
 
 from random import randint
 
-rule = input("Skriv om ni spelar udda eller jämnt:")
+rule = input("Skriv om ni spelar udda eller jämnt: ")
 computer = randint(1, 6)
 player = randint(1, 6)
-odd = (1, 3, 5)
-even = (2, 4, 6)
+
 if rule == "udda":
-    computer_result = computer in odd
-    player_result = player in odd
+    computer_result = computer % 2
+    player_result = player % 2
     if computer_result == player_result:
         print(f"Computer: {computer}, Player: {player}, It's a draw")
     elif computer_result:
         print(f"Computer: {computer}, Player: {player}, Computer wins")
     else:
         print(f"Computer: {computer}, Player: {player}, Player wins")
+if rule == "jämnt":
+    computer_result = computer % 2
+    player_result = player % 2
+    if computer_result == player_result:
+        print(f"Computer: {computer}, Player: {player}, It's a draw")
+    elif computer_result:
+        print(f"Computer: {computer}, Player: {player}, Player wins")
+    else:
+        print(f"Computer: {computer}, Player: {player}, Computer wins")
