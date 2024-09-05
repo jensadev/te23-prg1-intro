38c11c81e2af6dae5eb79c04f24db12846fe57da
Jens Andreasson <jens.andreasson@ntig.se>
2024-09-05 09:58:17 +0200

    städa

--- random-1.py
+++ random-1.py
@@ -5,29 +5,6 @@
 # undersök om datorns tärning är udda
 # undersök om spelarens tärning är udda
 
-
-# from random import randint
-
-# rule = input("Skriv om ni spelar udda eller jämnt:")
-# computer = randint(1, 6)
-# player = randint(1, 6)
-
-# if rule == "udda":
-#     # logiken för att bestämma vinnare om det är udda
-#     computer_result = computer % 2
-#     player_result = player % 2
-#     # if computer or player result is 0, number is even
-#     print(
-#         f"Datorn rullade {computer_result} och spelaren {player_result}, spelregeln var {rule}"
-#     )
-# else:
-#     computer_result = computer % 2
-#     player_result = player % 2
-#     # if computer or player result is 1, number is odd
-#     print(
-#         f"Datorn rullade {computer_result} och spelaren {player_result}, spelregeln var {rule}"
-#    )
-
 from random import randint
 
 rule = input("Skriv om ni spelar udda eller jämnt: ")
