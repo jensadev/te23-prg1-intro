1aa0ea65b22545fe80e58e4035f0dff156be729a
Jens Andreasson <jens.andreasson@ntig.se>
2024-09-12 08:55:53 +0200

    kung färdigt med bäst av 3

--- kung.py
+++ kung.py
@@ -9,23 +9,34 @@
 from random import randint
 
 play_game = "J"
-player_one_score = 0 #initiera innan loopen för att den ska kunna behålla värdet
-player_two_score = 0
+player_one_score = 0  # initiera innan loopen för att den ska kunna behålla värdet
+player_two_score = 0  # snake_case
 
 while play_game.upper() == "J":
     player_one_roll = randint(1, 6)
     player_two_roll = randint(1, 6)
 
     if player_one_roll > player_two_roll:
-        print(f"Spelare ETT vinner med tärningskastet: {player_one_roll} över {player_two_roll}.")
+        print(
+            f"Spelare ETT vinner med tärningskastet: {player_one_roll} över {player_two_roll}."
+        )
         player_one_score += 1
     elif player_two_roll > player_one_roll:
         print(f"Spelare TVÅ vinner med tärningskastet: {player_two_roll}.")
         player_two_score += 1
     else:
-        print(f"Ingen spelare vinner, det är oavgjort med tärningskastet: {player_one_roll}.")
+        print(
+            f"Ingen spelare vinner, det är oavgjort med tärningskastet: {player_one_roll}."
+        )
 
-    #play_game = input("Vill du spela igen? [J/N]: ")
+    if player_one_score >= 2:
+        print(f"Spelare ett vann med {player_one_score} över {player_two_score}.")
+        play_game = "n"
+    elif player_two_score >= 2:
+        print(f"Spelare två vann med {player_two_score} över {player_one_score}.")
+        play_game = "n"
+
+    # play_game = input("Vill du spela igen? [J/N]: ")
 
 # att göra, komma ihåg vilken spelare som har vunnit
 # spela tills vi har en bäst av tre
