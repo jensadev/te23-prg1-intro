6d27388959b81a19a5ac7898393ee3ff83aab57e
Jens Andreasson <jens.andreasson@ntig.se>
2024-10-17 10:06:15 +0200

    listor-for program lekton 17/10

---
+++ adventure.py
@@ -0,0 +1,32 @@
+# äventyret sparar vilken runda det är och du har 10 liv, du får 1 poäng för varje runda du klarar
+# Du är en gullig liten äventyrare som är ute och promenerar i vildmarken.
+# I din påse har du en karta över området och en kompass.
+# inventory = ["karta", "kompass"]
+# Du kan välja att titta i din påse, promenera vidare eller löka
+# Om du tittar i din påse får du reda på vad du har med dig
+# Om du promenerar vidare så fortsätter du din vandring
+# Om du lökar så står du stilla och tittar dig omkring
+# Vid lökning finns det en 50% chans att du hittar något spännande ["vättejos", "kantarell", "ekorre", "sten"]
+# det finns även en 20% chans att du möter en person på din vandring
+# Om du äter vättejos så blir du sjuk och förlorar 1 liv
+# Om du äter kantarell så blir du mätt och får 1 liv
+# Om du äter ekorre så är du moraliskt tvivelaktig och förlorar 1 poäng
+# Om du äter sten så bryter du dina tänder och förlorar 1 liv
+# Om du möter en person och är smittad av vätte så kan du bita personen och förvandla dem till en vätte, -10 poäng
+# du landar i den mörka sidan och spelet är över
+# Vad väljer du att göra?
+# Det är en 50% chans att du möter en vätte på din vandring
+# Plötsligt möter du en ondskefull vätte, de är väldigt fega, farliga och hemska
+# Vätten närmar sig hotfull, vad väljer du att göra?
+# Bekämpa vätten med pathos, logos och ethos alltså på svenska med känslor, logik och etik
+# logik besegrar känslor, du trasslar in vätten i en logisk diskussion och kan smita förbi
+# +1 poäng för att du klarade dig förbi vätten
+# etik besegrar logik, vätte imponeras av din moral och ger dig en gåva
+# +1 poäng för att du klarade dig förbi vätten
+# du får en slumpad gåva från vätten, ["ostkant", "pepparkvarn", "fiskespö", "tandpetare"]
+# känslor besegrar etik, känslorna tar över och vätten blir arg och biter dig, spring!
+# Du har blivit biten av vätten, vättar är giftiga, du kommer att förvandlas till en vätte om 3 rundor
+# Det är oavgjort, du tänker så hårt att du får huvudvärk
+# -1 liv
+
+# Du har klarat dig förbi vätten och kan fortsätta din vandring
--- listor-for.py
+++ listor-for.py
@@ -1,17 +1,19 @@
 print("Välkommen till det bästa programmet, där du sparar mat")
 run = True
+dishes = ["Tryffelsvin", "Gurksuffle", "Panikalladåb", "Saftsås"] # skapa listan utanför loopen
 while run:
     # lägga till ta bort maträtt
-    choice = input("Vad vill du göra? \n[1] Skriv ut\n[2] Lägg till mat\n[3] Avsluta\n")
+    choice = input("Vad vill du göra? \n[1] Skriv ut\n[2] Lägg till mat\n[3] Sortera\n[4] Avsluta\n")
     if choice == "1":
-        print("skriv ut")
-        dishes = ["Kroppkakor", "Stroganoff", "Pizza"]
+        print("Maträtterna är: ")
         for dish in dishes:
             print(dish)
     elif choice == "2":
         dish = input("Skriv in en ny maträtt: ")
         dishes.append(dish)
     elif choice == "3":
+        dishes.sort()
+    elif choice == "4":
         run = False
     else:
         print("Jag fattar inte ditt kommando")
