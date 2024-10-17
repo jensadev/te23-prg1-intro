d55f4a88a37369e29dd874f12607aa12c36568bb
Jens Andreasson <jens.andreasson@ntig.se>
2024-10-17 10:14:44 +0200

    kom ihåg

--- adventure.py
+++ adventure.py
@@ -6,13 +6,15 @@
 # Om du tittar i din påse får du reda på vad du har med dig
 # Om du promenerar vidare så fortsätter du din vandring
 # Om du lökar så står du stilla och tittar dig omkring
-# Vid lökning finns det en 50% chans att du hittar något spännande ["vättejos", "kantarell", "ekorre", "sten"]
+# När du lökar finns det en 50% chans att du hittar något spännande 
+# items = ["vättejos", "kantarell", "ekorre", "sten"]
 # det finns även en 20% chans att du möter en person på din vandring
 # Om du äter vättejos så blir du sjuk och förlorar 1 liv
 # Om du äter kantarell så blir du mätt och får 1 liv
 # Om du äter ekorre så är du moraliskt tvivelaktig och förlorar 1 poäng
 # Om du äter sten så bryter du dina tänder och förlorar 1 liv
-# Om du möter en person och är smittad av vätte så kan du bita personen och förvandla dem till en vätte, -10 poäng
+# Om du möter en person och är smittad av vätte så kan du bita personen och
+# förvandla dem till en vätte, -10 poäng
 # du landar i den mörka sidan och spelet är över
 # Vad väljer du att göra?
 # Det är en 50% chans att du möter en vätte på din vandring
@@ -30,3 +32,27 @@
 # -1 liv
 
 # Du har klarat dig förbi vätten och kan fortsätta din vandring
+
+
+import random
+
+def random_choice():
+    choice = random.randint(0, 100)
+    if choice <= 50:
+        return 0
+    elif choice <= 70:
+        return 1
+    else:
+        return 2
+
+# The patos logos and ethos function is a rock paper scissors game where logic
+# beats emotions, ethics beats logic and emotions beats ethics
+
+def patos_logos_ethos():
+    choice = random.randint(0, 2)
+    if choice == 0:
+        return "logik"
+    elif choice == 1:
+        return "etik"
+    else:
+        return "känslor"
